13b8e7715e1602c06bca32d3a4248da723c2e2a7
NZAMBOU Armel <armel.nzambou@elecit.local>
2024-07-16 19:21:57 +0200

    modif auth

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from account.models import CustomUser, Debit
+from account.models import CustomUser
 
 
 admin.site.register(CustomUser)
-admin.site.register(Debit)
+# admin.site.register(Debit)
